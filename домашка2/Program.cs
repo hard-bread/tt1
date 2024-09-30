4f425057a3624ceed9c821361bf7547cc7393b36
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-09-30 13:23:20 +0500

    Домашнее задание№2

---
+++ домашка2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace домашка2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите длину стороны квадрата: ");
+            double side = Convert.ToDouble(Console.ReadLine());
+
+            double perimeter = 4 * side;
+            double area = side * side;
+
+            Console.WriteLine("Сторона квадрата: " + side);
+            Console.WriteLine("Периметр квадрата: " + perimeter);
+            Console.WriteLine("Площадь квадрата: " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
