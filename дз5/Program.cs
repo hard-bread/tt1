55ba07ca73a376fea1537713600458409740c891
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-10-11 14:09:32 +0500

    дз5

---
+++ дз5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace дз5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double result = CalculateFunction(x);
+            Console.WriteLine($"f(x) = {result}");
+        }
+
+        static double CalculateFunction(double x)
+        {
+            return (2 * Math.Cos(1 / (x * x + 2))) / (x * x * x + 1);
+        }
+    }
+
+}
