35642bc9f165a61be0dc27e92c510e77bebef05a
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-10-12 22:27:47 +0500

    выделение метода

---
+++ выделение метода/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace выделение_метода
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = Func(3, 5)*Func(3, 7)*Func(5, 5);
+
+            Console.WriteLine("x = " + Math.Round(y, 3));
+
+            Console.ReadKey();
+        }
+        static double Func(double a, double b)
+        {
+            return (Math.Sin(a) + Math.Sqrt(a)) / (Math.Cos(b) + Math.Sqrt(b));
+        }
+    }
+}
