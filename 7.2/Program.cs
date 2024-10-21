7c51863999f076282ff2783e835858bf501a157f
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-10-21 13:20:27 +0500

    7.2

---
+++ 7.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки (x, y):");
+
+            Console.Write("x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool isInRegion = IsPointInRegion(x, y);
+
+            Console.WriteLine(isInRegion ? "True" : "False");
+        }
+
+        static bool IsPointInRegion(double x, double y)
+        {
+            if (x >= -3 && x <= -1)
+            {
+                return true;
+            }
+            else
+            {
+                return y >= -2;
+            }
+        }
+    }
+}
---
+++ ДЗ7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите m");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите n");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            bool proverka = (m % 2 == 0) ^ (n % 2 == 0);
+
+            Console.WriteLine(proverka);
+
+            Console.ReadKey();
+        }
+    }
+}
