02789fd6365ad487b3f03c46ef64b0ddef452712
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-10-22 23:34:35 +0500

    домашнее7.1

---
+++ домашнее7.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace домашнее7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое целое число m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе целое число n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Проверяем, является ли только одно из чисел четным
+            bool onlyOneEven = (m % 2 == 0) ^ (n % 2 == 0);
+
+            // Выводим результат
+            Console.WriteLine(onlyOneEven);
+        }
+    }
+}
