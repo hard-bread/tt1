7a0a76aa638e6c11b994df1e59d8a152b95d0705
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-10-22 23:46:11 +0500

    домашка7.1

---
+++ домашка777.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace домашка777._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое целое число m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе целое число n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            bool onlyOneEven = (m % 2 == 0) ^ (n % 2 == 0);
+
+            Console.WriteLine(onlyOneEven);
+        }
+    }
+}
