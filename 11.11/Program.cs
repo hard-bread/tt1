db73fda31eb6debd0a057c4008e6e38eaaa1d2be
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-11-11 12:59:52 +0500

    Задание №8

---
+++ 11.11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите значение x: ");
+            double x;
+
+            while (!double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.Write("Некорректный ввод. Пожалуйста, введите числовое значение x: ");
+            }
+
+            double result = CalculateFunction(x);
+            Console.WriteLine($"f({x}) = {result}");
+        }
+
+        static double CalculateFunction(double x)
+        {
+            double sinValue = Math.Sin(x);
+
+            if (sinValue < -0.5)
+            {
+                return sinValue;
+            }
+            else if (sinValue <= 0.5)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
+
