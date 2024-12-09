8e8ab454a790ff522372c1ed1d5803356cbbac3a
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-12-09 13:09:06 +0500

    o2

---
+++ o2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace o2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число n:");
+            int n = int.Parse(Console.ReadLine());
+            double product = 1.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите число a_{i + 1}:");
+                double a = double.Parse(Console.ReadLine());
+                product *= a;
+            }
+
+            Console.WriteLine($"Произведение: {product}");
+        }
+    }
+}
