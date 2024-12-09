9e84ebda4f9b20b81aa1ba2487f5021e6c5afcbf
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-12-09 13:26:52 +0500

    o6

---
+++ o6/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace o6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество сотрудников n:");
+            int n = int.Parse(Console.ReadLine());
+
+            double[,] salaries = new double[n, 3];
+            int[] maxSalaryMonth = new int[n];
+            double[] maxSalaryInMonths = new double[3];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите зарплату сотрудника {i + 1} за 3 месяца:");
+                Console.WriteLine("ВВЕДИТЕ 3 ЗНАЧЕНИЯ ЧЕРЕЗ ENTER");
+                for (int j = 0; j < 3; j++)
+                {
+                    salaries[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                double maxSalary = salaries[i, 0];
+                int monthIndex = 0;
+                for (int j = 1; j < 3; j++)
+                {
+                    if (salaries[i, j] > maxSalary)
+                    {
+                        maxSalary = salaries[i, j];
+                        monthIndex = j;
+                    }
+                }
+                maxSalaryMonth[i] = monthIndex;
+            }
+
+            for (int j = 0; j < 3; j++)
+            {
+                double maxSalary = salaries[0, j];
+
+                int employeeIndex = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (salaries[i, j] > maxSalary)
+                    {
+                        maxSalary = salaries[i, j];
+                        employeeIndex = i;
+                    }
+                }
+                maxSalaryInMonths[j] = maxSalary;
+                Console.WriteLine($"Самая высокая зарплата в месяце {j + 1}: Сотрудник {employeeIndex + 1}, Зарплата: {maxSalary}");
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Сотрудник {i + 1} получал самую высокую зарплату в месяце {maxSalaryMonth[i] + 1}");
+            }
+        }
+    }
+}
---
+++ o6/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("o6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("o6")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c59b420f-3fa8-4d4f-a70d-c30ef61ddb8f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
