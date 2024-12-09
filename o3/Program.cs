35c8c0cb4ba3987d24022a728bf1e44c6c4254fb
K.N.Tverdokhlebova@urfu.me <K.N.Tverdokhlebova@urfu.me>
2024-12-09 13:16:27 +0500

    о3

---
+++ o3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace o3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a (1 < a < 1,5): ");
+            Console.WriteLine("Например, 1,2. ДРОБНАЯ ЧАСТЬ ОТДЕЛЕТСЯ ЗАПЯТОЙ, А НЕ ТОЧКОЙ");
+            double a = double.Parse(Console.ReadLine());
+            int n = 1;
+
+            while (1 + 1.0 / n >= a)
+            {
+                Console.WriteLine(n); n++;
+            }
+        }
+    }
+}
